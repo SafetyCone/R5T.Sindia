3cbb8780124ac996a0a13866d0bc4fcfc9bbded9
MinexAutomation <minex.automation@gmail.com>
2019-11-22 14:16:11 -0800

    Added anomaly identity class.

---
+++ source/R5T.Sindia/Code/Classes/AnomalyIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Sindia.Code.Classes
+{
+    public class AnomalyIdentity : TypedGuid
+    {
+        #region Static
+
+        public static AnomalyIdentity New()
+        {
+            var anomalyIdentity = new AnomalyIdentity();
+            return anomalyIdentity;
+        }
+
+        public static AnomalyIdentity From(Guid guid)
+        {
+            var anomalyIdentity = new AnomalyIdentity(guid);
+            return anomalyIdentity;
+        }
+
+        #endregion
+
+
+        public AnomalyIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public AnomalyIdentity()
+            : this(Guid.NewGuid())
+        {
+        }
+    }
+}
